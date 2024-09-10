d8bba83a145b4cc8b5a789324c19f835da63ac06
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-10 15:37:50 +0900

    管理者商品一覧画面を作成してadminのrootに

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    root 'products#index'
+  end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
