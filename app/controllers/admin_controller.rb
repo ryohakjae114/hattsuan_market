2addf379fe516ae71ef665d7e942d1c11bd04e7c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-11 14:06:58 +0900

    Revert "管理者でログアウトできるように"

--- app/controllers/admin_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdminController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  layout 'admin'
-end
