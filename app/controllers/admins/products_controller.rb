649e473238017181953d0d7379d6722bd5b9eb51
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-12 14:43:35 +0900

    商品登録画面作成中

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -2,4 +2,11 @@ class Admins::ProductsController < ApplicationController
   def index
     @products = Product.order(:created_at).page(params[:page]).per(50)
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   namespace :admins do
     root 'products#index'
+    resources :products, only: %i[new create]
   end
-  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
