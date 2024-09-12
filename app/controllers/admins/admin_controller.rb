cd38b55388d99eca309bcc46df44b26d34eb1532
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-12 16:03:28 +0900

    rubocopの警告を修正

--- app/controllers/admins/admin_controller.rb
+++ app/controllers/admins/admin_controller.rb
@@ -1,4 +1,4 @@
-class AdminController < ActionController::Base
+class Admins::AdminController < ApplicationController
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
