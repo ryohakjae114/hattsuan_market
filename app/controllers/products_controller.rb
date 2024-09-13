7a00d5186c3cb00c56e61a11c30690246aae9186
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-13 15:39:02 +0900

    商品詳細機能を作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
     @products = Product.order(:created_at).page(params[:page]).per(12)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
