214142bad3493fbbe8883de6b1ee9a9077d92de4
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-17 14:19:14 +0900

    migration ファイルの修正 & rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_025325) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_17_051642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_025325) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "purchase_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_025325) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "purchases", "users"
 end
