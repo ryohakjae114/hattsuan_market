4f430fc9126dfaabcfb5451f790484437827ecf0
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-17 14:22:42 +0900

    モデルの修正

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,3 @@
 class Purchase < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+  has_many :purchases, dependent: :destroy
 end
