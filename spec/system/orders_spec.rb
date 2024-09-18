b711df06e5049aef9c59b64187aaec061deb41f2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-18 15:02:33 +0900

    カート追加機能のテスト作成

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Orders', type: :system do
+  context 'ログイン時' do
+    let!(:hakjae) { create(:user, email: 'hattsuan@example.com', password: '123456123456', password_confirmation: '123456123456') }
+    let!(:nerune) { create :product, name: 'ねるねるねーるね', price_without_tax: 100 }
+
+    before do
+      sign_in hakjae
+    end
+
+    it '商品をカートに追加できる' do
+      visit product_path(nerune)
+      fill_in '個数',	with: 2
+      click_on 'カートに追加'
+      expect(page).to have_content '新規登録しました'
+      visit order_items_path
+      expect(page).to have_content 'ねるねるねーるね'
+      expect(page).to have_content '220円'
+    end
+  end
+end
