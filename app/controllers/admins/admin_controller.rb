ae87ab53486a1488e28000a7a9e5f9067fcdcc8e
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-18 14:21:24 +0900

    ログインしてないとカート機能は使えない

--- app/controllers/admins/admin_controller.rb
+++ app/controllers/admins/admin_controller.rb
@@ -1,4 +1,5 @@
 class Admins::AdminController < ApplicationController
+  skip_before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
