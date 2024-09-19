50391b9f469e6e867d80d227147d53ad9d9fda3c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-19 11:57:56 +0900

    cart_itemにvalidateを追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def price_with_tax
     product.price_with_tax * quantity
   end
