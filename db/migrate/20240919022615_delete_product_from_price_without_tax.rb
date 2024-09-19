39f9bdf6322000b2477be2f69203ced32249d46b
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-19 15:39:33 +0900

    マイグレーションファイルの名前修正

--- db/migrate/20240919022615_delete_product_from_price_without_tax.rb
+++ db/migrate/20240919022615_delete_product_from_price_without_tax.rb
@@ -1,4 +1,4 @@
-class DeleteProductFromCartItem < ActiveRecord::Migration[7.2]
+class DeleteProductFromPriceWithoutTax < ActiveRecord::Migration[7.2]
   def change
     remove_column :cart_items, :price_without_tax, :integer
   end
