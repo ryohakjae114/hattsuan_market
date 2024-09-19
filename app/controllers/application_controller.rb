895d29bb1239ba9a8a4c5a88c95f31bc605633c0
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-19 10:54:12 +0900

    current_cartメソッドの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_cart
+    Cart.find_or_create_by(user_id: current_user.id)
+  end
 end
