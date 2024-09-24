93fe4b66d5925400a5e7dae53c4aa6fbaf9e5a9f
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-24 13:35:06 +0900

    注文画面を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,19 @@ class OrdersController < ApplicationController
   def new
     @order = current_user.orders.build
   end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    if @order.save
+      redirect_to root_path, notice: t('.ordered'), status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:delivery_on, :delivery_time_zone, :delivery_address, :addressee_name)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,12 @@
 class Order < ApplicationRecord
   extend Enumerize
 
-  AVAILABLE_DELIVERY_TIME_ZONES = %w[08-12 12-14 14-16 16-18 18-20 20-21]
+  AVAILABLE_DELIVERY_TIME_ZONES = %w[08-12 12-14 14-16 16-18 18-20 20-21].freeze
   enumerize :delivery_time_zone, in: AVAILABLE_DELIVERY_TIME_ZONES
 
   belongs_to :user
 
-  validates :delivery_on, presence: true, comparison: { greater_than: -> { Time.zone.today } }
+  validates :delivery_on, presence: true, comparison: { greater_than_or_equal_to: -> { Time.zone.today } }
   validates :delivery_time_zone, presence: true
   validates :delivery_address, presence: true, length: { maximum: 200 }
   validates :addressee_name, presence: true, length: { maximum: 50 }
