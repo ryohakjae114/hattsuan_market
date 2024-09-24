222c817e1a44becd8f322ee98ada8aca328c14ac
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-24 15:01:54 +0900

    テストのリファクタリング

--- spec/system/cart_items_spec.rb
+++ spec/system/cart_items_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'CartItems', type: :system do
     let!(:hakjae) { create(:user, email: 'hattsuan@example.com') }
     let!(:hakjae_cart) { create(:cart, user: hakjae) }
     let!(:nerune) { create :product, name: 'ねるねるねーるね', price_without_tax: 100 }
+    let!(:poteti) { create :product, name: 'ポテチ', price_without_tax: 200 }
+    let!(:cart_item) { create(:cart_item, cart: hakjae_cart, product_id: poteti.id, quantity: 5) }
 
     before do
       sign_in hakjae
@@ -21,15 +23,15 @@ RSpec.describe 'CartItems', type: :system do
     end
 
     it 'カートに入った商品の個数を変更できる' do
-      create(:cart_item, cart: hakjae_cart, product_id: nerune.id, quantity: 5)
-      visit product_path(nerune)
+      visit product_path(poteti)
       expect(page).to have_field '個数', with: 5
-      fill_in '個数', with: 10
+      fill_in '個数', with: 5
       click_on '数量を変更'
       expect(page).to have_content '更新しました'
       visit cart_path
-      within '.ねるねるねーるね' do
-        expect(page).to have_content '10'
+      within ".cart_item_#{cart_item.id}" do
+        expect(page).to have_content 'ポテチ'
+        expect(page).to have_content '5'
       end
     end
   end
