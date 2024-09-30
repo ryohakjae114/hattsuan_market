6dd31cce9d2ad5a57b5b870f53231d717fb7b1e2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-09-30 14:39:56 +0900

    境界テストを修正

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,13 +6,16 @@ RSpec.describe Order, type: :model do
     let!(:order) { build(:order, user:) }
 
     example '配達日は配達3営業日から15営業日までの間で指定できる' do
-      order.delivery_on = 2.business_day.from_now
+      order.delivery_on = 3.business_day.from_now.to_date - 1
       expect(order).not_to be_valid
 
-      order.delivery_on = 10.business_day.from_now
+      order.delivery_on = 3.business_day.from_now.to_date
       expect(order).to be_valid
 
-      order.delivery_on = 16.business_day.from_now
+      order.delivery_on = 15.business_day.from_now.to_date
+      expect(order).to be_valid
+
+      order.delivery_on = 15.business_day.from_now.to_date + 1
       expect(order).not_to be_valid
     end
   end
