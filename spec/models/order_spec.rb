e23b6c8742b0da4b894fae6c67ec5b62468e2246
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-02 15:01:55 +0900

    代引き手数料のモデルテスト作成

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -32,13 +32,43 @@ RSpec.describe Order, type: :model do
 
     describe 'delivery_fee' do
       example '商品の合計が0円から10000円未満だと300円の代引き手数料がかかる' do
-        product.update!(price_without_tax: (9999 / (110.0 / 100.0)).floor)
-        puts product.price_without_tax
+        product.update!(price_without_tax: (9999 / (110.0 / 100.0)).ceil)
         create :cart_item, cart:, product:, quantity: 1
-        puts cart.cart_items.first.price_with_tax
         order.save!
-        puts order.order_items.first.price_with_tax
         expect(order.delivery_fee).to eq 300
+      end
+
+      example '商品の合計が10000円から30000円未満だと400円の代引き手数料がかかる' do
+        product.update!(price_without_tax: (10000 / (110.0 / 100.0)).ceil)
+        create :cart_item, cart:, product:, quantity: 1
+        order.save!
+        expect(order.delivery_fee).to eq 400
+
+        product.update!(price_without_tax: (29999 / (110.0 / 100.0)).ceil)
+        cart = user.create_cart!
+        create :cart_item, cart:, product:, quantity: 1
+        order.save!
+        expect(order.delivery_fee).to eq 400
+      end
+
+      example '商品の合計が30000円から100000円未満だと600円の代引き手数料がかかる' do
+        product.update!(price_without_tax: (30000 / (110.0 / 100.0)).ceil)
+        create :cart_item, cart:, product:, quantity: 1
+        order.save!
+        expect(order.delivery_fee).to eq 600
+
+        product.update!(price_without_tax: (99999 / (110.0 / 100.0)).ceil)
+        cart = user.create_cart!
+        create :cart_item, cart:, product:, quantity: 1
+        order.save!
+        expect(order.delivery_fee).to eq 600
+      end
+
+      example '商品の合計が100000以上だと1000円の代引き手数料がかかる' do
+        product.update!(price_without_tax: (100000 / (110.0 / 100.0)).ceil)
+        create :cart_item, cart:, product:, quantity: 1
+        order.save!
+        expect(order.delivery_fee).to eq 1000
       end
     end
   end
