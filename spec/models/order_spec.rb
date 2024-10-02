adf8ae13b98b2d5f527d3eea068470de6a48ae51
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-02 11:11:21 +0900

    送料を計算するロジックのモデルテストの作成

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe 'validation' do
     let!(:user) { create(:user) }
+    let!(:product) { create(:product) }
+    let!(:cart) { create(:cart, user:) }
     let!(:order) { build(:order, user:) }
 
     example '配達日は配達3営業日から14営業日までの間で指定できる' do
@@ -17,6 +19,12 @@ RSpec.describe Order, type: :model do
 
       order.delivery_on = 14.business_day.from_now.to_date + 1
       expect(order).not_to be_valid
+    end
+
+    example '送料は5つにつき600円かかる' do
+      create :cart_item, cart:, product:, quantity: 5
+      order.save!
+      expect(order.postage).to eq 600
     end
   end
 end
